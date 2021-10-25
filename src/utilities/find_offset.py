4ef4241db14bada4dcc1fde7d500b2fdcb515b9b
CodingInPython-OSRS <58365905+CodingInPython-OSRS@users.noreply.github.com>
2021-10-25 22:30:47 +0200

    Update find_offset.py

--- src/utilities/find_offset.py
+++ src/utilities/find_offset.py
@@ -2,10 +2,10 @@ from window_capture import WindowCapture
 import pyautogui
 
 # Configuration
-client_top_border = 30
-client_side_border = 50
-offset_minimap_x = 102
-offset_minimap_y = 110
+client_top_border = 0
+client_side_border = 0
+offset_minimap_x = 0
+offset_minimap_y = 0
 
 window = WindowCapture(client_top_border,
                         client_side_border,
@@ -15,6 +15,6 @@ window = WindowCapture(client_top_border,
 box = window.get_window('Runelite')
 coords = window.get_center_minimap(box)
 print(coords)
-# pyautogui.displayMousePosition()
-pyautogui.click(coords)
-# 2218 495
+pyautogui.displayMousePosition()
+# pyautogui.click(coords)
+# 

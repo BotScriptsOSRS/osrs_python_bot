28aa8485e16bfbe068d5b56d3e21d5598088104e
CodingInPython-OSRS <58365905+CodingInPython-OSRS@users.noreply.github.com>
2021-11-03 13:50:19 +0100

    Update tanning.py

--- src/tanning_bot/tanning.py
+++ src/tanning_bot/tanning.py
@@ -21,8 +21,10 @@ class Tanning(Walking):
                     pyautogui.moveTo(coords_trade[0][0], coords_trade[0][1], duration = 0.1)
                     pyautogui.click()
                     coords_tan = self.locate_image_on_screen(self.tanned_hide[3], 0.99)
-                    while coords_tan == [] or coords_tan == None:
+                    while coords_tan == []:
                         coords_tan = self.locate_image_on_screen(self.tanned_hide[3], 0.99)
+                        continue
+                    if coords_tan[0] == None:
                         continue
                     return coords_tan[0]
                 else:

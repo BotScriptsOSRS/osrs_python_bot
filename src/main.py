c21496a16de9d59eda9b5cf3f7665dd0cb2a4e91
Cronuh <kkomendar@gmail.com>
2022-08-22 00:48:12 +0200

    gitignore

--- src/main.py
+++ src/main.py
@@ -6,33 +6,31 @@ from utilities.object_detection import ObjectDetection
 from walker.path_enum import Path
 from walker.walking import Walking
 
-# hide = Hide.COW_HIDE.value
-# tanned_hide = Hide.HARD_LEATHER.value
+hide = Hide.COW_HIDE.value
+tanned_hide = Hide.HARD_LEATHER.value
 
-# breaker = TakeBreak()
-# tan_bot = TanBot(hide, tanned_hide)
-
+breaker = TakeBreak()
+tan_bot = TanBot(hide, tanned_hide)
 
 # breaker.login()
 # python src/utilities/server.py
 # pip install simplejson numpy pywin32 pyautogui opencv-python pillow
-walker = Walking(data, ObjectDetection)
+
+walker = Walking()
 
 def main():
-    path = Path.GE_CIRCLE.value
-    walker.walk(path)
 
-    # time_start = time.time()
-    # time_duration_min = random.randint(120, 180)
+    time_start = time.time()
+    time_duration_min = random.randint(120, 180)
 
-    # while True:
-    #     # Run script for 2 to 3 hours.
-    #     while time.time() < time_start + time_duration_min*60:
-    #         tan_bot.run()
-    #     tan_bot.inv_open = False
-    #     # Take a break of 5 to 15 minutes.
-    #     breaker.take_break()
-    #     time_start = time.time()
+    while True:
+        # Run script for 2 to 3 hours.
+        while time.time() < time_start + time_duration_min*60:
+            tan_bot.run()
+        tan_bot.inv_open = False
+        # Take a break of 5 to 15 minutes.
+        breaker.take_break()
+        time_start = time.time()
 
 if __name__ == '__main__':
     main()
